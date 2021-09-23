a63d81c548df744ffa82688b87a8fd658a235599
Sethph <seth.hammond@go.tarleton.edu>
2021-09-23 16:18:31 -0500

    The program will randomally choose questions until all questions have been choosen

--- src/main/java/com/mycompany/lab6/Main.java
+++ src/main/java/com/mycompany/lab6/Main.java
@@ -22,33 +22,35 @@ import java.util.Scanner;
  * @author 000728303
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("questions.txt");
         Charset cs = Charset.forName("UTF-8");
-        List<String> lines = Files.readAllLines(path,cs);
-        List<Question> questions= new ArrayList<>();
-        
+        List<String> lines = Files.readAllLines(path, cs);
+        List<Question> questions = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
         //This reads in the file and makes a new question object
-        for(String line : lines){
-            String []s = line.split(";");
+        for (String line : lines) {
+            String[] s = line.split(";");
             int id = Integer.parseInt(s[1]);
-            Question question = new Question(s[0],id);
+            Question question = new Question(s[0], id);
             questions.add(question);
         }
-        
-        //this chooses a random question and prints it
-        int questionNum = questions.size();
-        int randomNum = (int) (Math.random() * (questionNum));
-        System.out.println(questions.get(randomNum).toString());
-        
-        //this allows the user to write a answer into a text file
-        try(PrintWriter out = new PrintWriter(new OutputStreamWriter
-        (new FileOutputStream("anwser.txt"),"UTF-8"))){
-            Scanner sc = new Scanner(System.in);
-            String answer = sc.nextLine();
-            out.println(answer);
+
+        try ( PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream("anwser.txt"), "UTF-8"))) {
+            while (questions.size() != 0) {
+                //this chooses a random question and prints it
+                int questionNum = questions.size();
+                int randomNum = (int) (Math.random() * (questionNum));
+                System.out.println(questions.get(randomNum).toString());
+                questions.remove(randomNum);
+                
+                //this allows the user to write a answer into a text file
+                String answer = sc.nextLine();
+                out.println(answer);
+            }
         }
-        
-        
+
     }
 }
